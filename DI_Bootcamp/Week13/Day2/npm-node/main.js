f4768354e93b8463b1e5fc5f9e1efdac108f4ebd
HansJT <teevij0305@gmail.com>
2023-01-10 13:13:47 -0800

    newcommit_Week13_Day2_ExerciseXP

---
+++ DI_Bootcamp/Week13/Day2/npm-node/main.js
@@ -0,0 +1,19 @@
+// Exercise 1
+
+// const a = 5;
+// const b = 10;
+
+// console.log(`Hello you are ${a+b}`)
+
+
+// Exercise 2
+// console.log('Hi! How are you ?');
+
+
+// Exercise 3
+// let x;
+// const c = 6;
+// x = a + b;
+// console.log(x);
+// console.log(x+c);
+
